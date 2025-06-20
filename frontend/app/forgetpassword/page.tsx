a88bf5910e002f5c98049e179ff73e6ecd7b35ed
Inverated <120025059+Inverated@users.noreply.github.com>
2025-06-21 01:23:20 +0800

    React suspense? issue
    Apologies for forgetting to switch branch before doing all these commits...

--- frontend/app/forgetpassword/page.tsx
+++ frontend/app/forgetpassword/page.tsx
@@ -2,10 +2,10 @@
 
 import { supabase } from "@/lib/supabase";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react"
+import { Suspense, useEffect, useRef } from "react"
 import { CiUser } from "react-icons/ci";
 
-export default function ForgetPassword() {
+function ForgetPassword() {
     const emailInput = useRef<HTMLInputElement>(null)
     const searchParams = useSearchParams();
     const messageElement = useRef<HTMLElement>(null)
@@ -33,43 +33,50 @@ export default function ForgetPassword() {
                         emailInput.current.value
                 }
             }
-            
+
         } else {
             if (messageElement.current) {
                 messageElement.current.textContent = 'Please input a valid email address'
             }
         }
     }
-
     return (
-        <div className="flex justify-center-safe items-center h-screen">
-            <div>
-                <div className="text-center">
-                    <div className="text-3xl font-bold">Forget your password</div>
-                    <span>You will receive an email to reset your password</span>
-                </div>
-                <form action={forgetPassword}>
-                    <div className="my-3 flex bg-gray-300 rounded-lg">
-                        <CiUser className="text-2xl" />
-                        <input
-                            type="email"
-                            name="forgetPasswordEmail"
-                            placeholder="example@email.com"
-                            ref={emailInput}
-                            className="mx-2 bg-transparent w-full" />
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex justify-center-safe items-center h-screen">
+                <div>
+                    <div className="text-center">
+                        <div className="text-3xl font-bold">Forget your password</div>
+                        <span>You will receive an email to reset your password</span>
                     </div>
-                    <div>
-                        <button type="submit" className="bg-black active:bg-gray-900 active:scale-95 w-full transition cursor-pointer text-white p-2 rounded-lg">
-                            Confirm
-                        </button>
+                    <form action={forgetPassword}>
+                        <div className="my-3 flex bg-gray-300 rounded-lg">
+                            <CiUser className="text-2xl" />
+                            <input
+                                type="email"
+                                name="forgetPasswordEmail"
+                                placeholder="example@email.com"
+                                ref={emailInput}
+                                className="mx-2 bg-transparent w-full" />
+                        </div>
+                        <div>
+                            <button type="submit" className="bg-black active:bg-gray-900 active:scale-95 w-full transition cursor-pointer text-white p-2 rounded-lg">
+                                Confirm
+                            </button>
+                        </div>
+                    </form>
+                    <div className="my-2 text-sm w-sm">
+                        <p id='message'></p>
                     </div>
-                </form>
-                <div className="my-2 text-sm w-sm">
-                    <p id='message'></p>
                 </div>
             </div>
-        </div>
+        </Suspense>
     )
+}
 
-
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ForgetPassword />
+    </Suspense>
+  );
 }
