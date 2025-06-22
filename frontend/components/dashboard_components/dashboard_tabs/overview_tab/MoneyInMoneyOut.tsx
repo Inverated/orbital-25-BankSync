aba5789cbb9802ea4ba9a30d2b6d7d2cfab292a2
Kristen Tan <kristan2605@gmail.com>
2025-06-23 01:06:59 +0800

    Created the feature where when an account is clicked on the overview tab, it will expand to show a bar chart of the money going in and out of the account for the past few months

---
+++ frontend/components/dashboard_components/dashboard_tabs/overview_tab/MoneyInMoneyOut.tsx
@@ -0,0 +1,11 @@
+interface MoneyInMoneyOutProps {
+    account_no: string,
+}
+
+export default function MoneyInMoneyOut({ account_no } : MoneyInMoneyOutProps) {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
