0883e89886619b0ea42608539edf5afc5dae3420
Inverated <120025059+Inverated@users.noreply.github.com>
2025-07-21 23:16:14 +0800

    bugfix login redirect failure
    Redirect from supabase not working after using oauth to sign in. Email sign in works

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,9 +3,36 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { ChartNoAxesCombined, FolderOutput, LayoutDashboard, MousePointer2, ShieldUser } from "lucide-react";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 export default function Home() {
     const router = useRouter()
+
+    useEffect(() => {
+        const hashParams = new URLSearchParams(window.location.hash.substring(1))
+
+        const access_token = hashParams.get('access_token')
+        const refresh_token = hashParams.get('refresh_token')
+
+        const signIn = async (access_token:string, refresh_token:string) => {
+            const { error } = await supabase.auth.setSession({
+                access_token,
+                refresh_token,
+            })
+
+            if (error) {
+                console.error('Session setup failed:', error.message)
+            } else {
+                router.push('/dashboard')
+            }
+        }
+        if (access_token && refresh_token) {
+            signIn(access_token, refresh_token)
+        }
+
+        window.history.replaceState(null, '', window.location.pathname)
+    }, [])
 
     return (
         <div>
@@ -35,7 +62,7 @@ export default function Home() {
                 <h1 className="text-6xl font-sans font-bold">Welcome to</h1>
 
                 <Image src="/name.png" alt="BankSync" width={500} height={250} className="mx-auto" />
-                
+
                 <p className="text-lg text-gray-500 w-3/8 mx-auto">
                     A simple, centralized platform to manage all your finances.
                 </p>
@@ -50,25 +77,25 @@ export default function Home() {
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 px-45">
                     <div className="bg-white shadow-xl p-8 flex flex-col items-center justify-center border border-green-500 border-2 rounded-3xl">
-                        <LayoutDashboard className="h-12 w-12 text-green-500 mb-3"/>
+                        <LayoutDashboard className="h-12 w-12 text-green-500 mb-3" />
                         <h2 className="text-2xl font-bold mb-2">Unified Dashboard</h2>
                         <p className="text-gray-500 text-center w-3/4">Upload your monthly bank statements and combine them into one centralized overview</p>
                     </div>
 
                     <div className="bg-white shadow-xl p-8 flex flex-col items-center justify-center border border-green-500 border-2 rounded-3xl">
-                        <FolderOutput className="h-12 w-12 text-green-500 mb-3"/>
+                        <FolderOutput className="h-12 w-12 text-green-500 mb-3" />
                         <h2 className="text-2xl font-bold mb-2">Seamless Data Export</h2>
                         <p className="text-gray-500 text-center w-3/4">Download statements for offline access or sharing</p>
                     </div>
 
                     <div className="bg-white shadow-xl p-8 flex flex-col items-center justify-center border border-green-500 border-2 rounded-3xl">
-                        <ShieldUser className="h-12 w-12 text-green-500 mb-3"/>
+                        <ShieldUser className="h-12 w-12 text-green-500 mb-3" />
                         <h2 className="text-2xl font-bold mb-2">Secure Authentication</h2>
                         <p className="text-gray-500 text-center w-3/4">Enjoy safe login with Google or GitHub, backed by robust encryption to protect your financial data</p>
                     </div>
 
                     <div className="bg-white rounded-3xl shadow-xl p-8 flex flex-col items-center border border-green-500 border-2 items-center justify-center">
-                        <ChartNoAxesCombined className="h-12 w-12 text-green-500 mb-3"/>
+                        <ChartNoAxesCombined className="h-12 w-12 text-green-500 mb-3" />
                         <h2 className="text-2xl font-bold mb-2">Meaningful Spending Insights</h2>
                         <p className="text-gray-500 text-center w-3/4">Visualise when and where your money is being spent</p>
                     </div>
@@ -79,10 +106,10 @@ export default function Home() {
                 <h1 className="text-4xl font-sans font-bold mb-8">Supported Banks</h1>
 
                 <p className="text-lg text-gray-500 w-3/8 mx-auto">
-                    BankSync currently supports the uploading of statements from the banks below. 
+                    BankSync currently supports the uploading of statements from the banks below.
                 </p>
 
-                <p className="text-lg text-gray-500 w-3/8 mx-auto mb-4"> 
+                <p className="text-lg text-gray-500 w-3/8 mx-auto mb-4">
                     More will be added soon!
                 </p>
 
@@ -119,7 +146,7 @@ export default function Home() {
 
             <footer className="bg-white pt-5 pb-5 flex items-center justify-center text-center">
                 <p className="text-md text-gray-500">
-                    Copyright &copy; { new Date().getFullYear()} Orbital &middot; BankSync | All rights reserved.
+                    Copyright &copy; {new Date().getFullYear()} Orbital &middot; BankSync | All rights reserved.
                 </p>
             </footer>
         </div>
