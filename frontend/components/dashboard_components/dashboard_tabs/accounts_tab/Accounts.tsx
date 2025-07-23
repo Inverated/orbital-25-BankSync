4612c6b846d74fdb6016e661fa1b5c13835d2d94
Kristen Tan <kristan2605@gmail.com>
2025-07-23 17:16:03 +0800

    Created additional feature for Accounts tab

--- frontend/components/dashboard_components/dashboard_tabs/accounts_tab/Accounts.tsx
+++ frontend/components/dashboard_components/dashboard_tabs/accounts_tab/Accounts.tsx
@@ -1,28 +1,55 @@
 import { useEffect, useState } from "react";
 import { Account } from "@/utils/types";
-import AccountsExpand from "./AccountsExpand";
+import AccountsExpandTransactions from "./AccountsExpandTransactions";
 import { useDatabase } from "@/context/DatabaseContext";
 
 export default function Accounts() {
     const [currAccounts, setAccounts] = useState<Account[]>([]);
 
-    const { accounts } = useDatabase()
+    const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+    const { accounts } = useDatabase();
 
     useEffect(() => {
         setAccounts(accounts);
     }, [accounts])
 
+    const toggleExpand = (index: number) => {
+        setExpandedIndex(prev => (prev === index ? null : index));
+    }
+
     return (
-        <div className="items-center justify-center">
-            {currAccounts.map((account, index) => (
-                <div className="rounded-lg p-7 m-5 border border-black space-y-1" key={index}>
-                    <h1>{account.bank_name}</h1>
-                    <p>Account Name: {account.account_name}</p>
-                    <p>Account No.: {account.account_no}</p>
-                    <p>Balance: ${account.balance.toFixed(2)}</p>
-                    <AccountsExpand account={account} index={index} />
-                </div>
-            ))}
+        <div className="flex justify-center">
+            <div className="w-3/4">
+                {currAccounts.map((account, index) => (
+                    <div 
+                        className="rounded-lg p-7 m-5 border border-gray-300 border-2 space-y-1 hover:cursor-pointer" 
+                        key={index}
+                        onClick={() => toggleExpand(index)}
+                    >
+                        {expandedIndex === index ? (
+                            <div className="text-lg flex flex-col gap-2">
+                                <p><a className="font-bold">Account Name:</a> {account.account_name}</p>
+                                <p><a className="font-bold">Account Number:</a> {account.account_no}</p>
+                                <p><a className="font-bold">Bank Name:</a> {account.bank_name}</p>
+                                <p><a className="font-bold">Balance:</a> ${account.balance.toFixed(2)}</p>
+                                
+                                <AccountsExpandTransactions account={account} index={index} />
+                            </div>
+                        ) : (
+                            <div className="flex flex-row items-center justify-between">
+                                <div className="text-xl font-bold">
+                                    {account.account_name} ({account.account_no}) &middot; {account.bank_name}
+                                </div>
+                                
+                                <div className="text-xl">
+                                    ${account.balance.toFixed(2)}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
